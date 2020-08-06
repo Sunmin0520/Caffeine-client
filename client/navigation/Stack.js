4bbb43603f90cb09beb8aa944a07c26c48278015
aiora32 <uukel32@gmail.com>
2020-08-06 20:33:24 +0900

    fix routing stack

--- client/navigation/Stack.js
+++ client/navigation/Stack.js
@@ -19,35 +19,35 @@ import Bookmark from "../pages/cafe/Bookmark";
 
 const Stack = createStackNavigator();
 
-export default () => (
-  //screen은 컴포넌트가 필요하다. 꼭 이름을 주어야한다.
-  //Stack.Navigator가 항상 하위 Stack.Screen에게 prop(navigation)을 준다.
-  //Navigator의 모든 screen 들은 navigation이란 prop에 접근권을 가지고 있다.
-  //component는 불려지고 screen은 props를 보낸다.
-  //stack안에 tabs들이 있도록 해주었다.
-  //screenOptions는 모든 screen에 대한 style을 정할 수있다.
-  <Stack.Navigator
-    screenOptions={{
-      headerStyle: {},
-      headerTintColor: "#fff",
-      headerBackTitleVisible: false,
-    }}
-  >
-    <Stack.Screen name="UserInfo" component={UserInfo} />
-    <Stack.Screen name="Signin" component={Signin} />
-    <Stack.Screen name="Signup" component={Signup} />
-    <Stack.Screen name="Main" component={Main} />
-    <Stack.Screen name="Tab" component={Tabs} />
-    <Stack.Screen name="Notelist" component={Notelist} />
-    <Stack.Screen name="Noteinfo" component={Noteinfo} />
-    <Stack.Screen name="Modifynote" component={Modifynote} />
-    <Stack.Screen name="Addnote" component={Addnote} />
+export default function Stacks() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {},
+        headerTintColor: "#fff",
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen name="UserInfo" component={UserInfo} />
+      <Stack.Screen name="Signin" component={Signin} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Tab" component={Tabs} />
+      {/* 
+    
     <Stack.Screen name="Regionlist" component={Regionlist} />
     <Stack.Screen name="Region" component={Region} />
     <Stack.Screen name="Cafeinfo" component={Cafeinfo} />
     <Stack.Screen name="Addreview" component={Addreview} />
     <Stack.Screen name="addcafeScreen" component={addcafeScreen} />
     <Stack.Screen name="Addcafe" component={Addcafe} />
-    <Stack.Screen name="Bookmark" component={Bookmark} />
-  </Stack.Navigator>
-);
+    <Stack.Screen name="Bookmark" component={Bookmark} /> */}
+    </Stack.Navigator>
+  );
+}
+//screen은 컴포넌트가 필요하다. 꼭 이름을 주어야한다.
+//Stack.Navigator가 항상 하위 Stack.Screen에게 prop(navigation)을 준다.
+//Navigator의 모든 screen 들은 navigation이란 prop에 접근권을 가지고 있다.
+//component는 불려지고 screen은 props를 보낸다.
+//stack안에 tabs들이 있도록 해주었다.
+//screenOptions는 모든 screen에 대한 style을 정할 수있다.
