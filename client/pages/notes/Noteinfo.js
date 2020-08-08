1c240534d049ee0981d31ebf9c7fb31818f389eb
aiora32 <uukel32@gmail.com>
2020-08-08 22:03:17 +0900

    add noteinfo css

--- client/pages/notes/Noteinfo.js
+++ client/pages/notes/Noteinfo.js
@@ -8,6 +8,7 @@ import {
   Alert,
   Modal,
   TouchableHighlight,
+  ScrollView,
 } from "react-native";
 import StarRating from "react-native-star-rating";
 import axios from "axios";
@@ -108,133 +109,222 @@ const Noteinfo = ({ navigation, route }) => {
   }, []);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#F8F8F5" }}>
-      <Text>{name}</Text>
-      <Text>{origin}</Text>
-      <Text>{mall}</Text>
-      <Text>{price}</Text>
-      <Text>{feature}</Text>
-      {/* <Text>{flavor}</Text> */}
-      {/* {console.log(arrayflavor)} */}
-      <View style={styles.container}>
-        {arrayflavor
-          .filter((data) => {
-            for (let i = 0; i < flavor.length; i++) {
-              if (flavor[i] === data.id) {
-                return data.name;
-              }
-            }
-          })
-          .map((result) => (
-            <TouchableOpacity
-              key={result.id}
-              style={{
-                alignItems: "center",
-                borderWidth: 1,
-                width: 80,
-                padding: 2,
-                //flavor onpress를 눌러서 flavor 함수실행 함수는 notes의 모든 노트를 불러온다.
-              }}
-              // onPress={() => {
-              //   filteringFlavor(result.id);
-              // }}
-            >
-              <Text>{result.name}</Text>
-            </TouchableOpacity>
-          ))}
+    <View style={styles.container}>
+      <View style={styles.container2}>
+        <Text style={styles.headtextstyle}>{name}</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.container3}>
+          <Text style={styles.topText}>원산지</Text>
+          <Text style={styles.bottomText}>{origin}</Text>
+        </View>
+        <View style={styles.container3}>
+          <Text style={styles.topText}>구입한 곳</Text>
+          <Text style={styles.bottomText}>{mall}</Text>
+        </View>
+        <View style={styles.container3}>
+          <Text style={styles.topText}>가격</Text>
+          <Text style={styles.bottomText}>{price}원 / 100g </Text>
+        </View>
+        <View style={styles.container3}>
+          <Text style={styles.topText}>특징</Text>
+          <Text style={styles.bottomText}>{feature}</Text>
+        </View>
+        <View style={styles.container3}>
+          <Text style={styles.topText}>플레이버 노트</Text>
+          <View style={styles.container4}>
+            {arrayflavor
+              .filter((data) => {
+                for (let i = 0; i < flavor.length; i++) {
+                  if (flavor[i] === data.id) {
+                    return data.name;
+                  }
+                }
+              })
+              .map((result) => (
+                <TouchableOpacity
+                  key={result.id}
+                  style={styles.flavorbutton}
+                  // onPress={() => {
+                  //   filteringFlavor(result.id);
+                  // }}
+                >
+                  <Text>{result.name}</Text>
+                </TouchableOpacity>
+              ))}
+          </View>
+        </View>
+
+        <View style={styles.container3}>
+          <Text style={styles.topText}>평점</Text>
+          <StarRating
+            disabled={true}
+            maxStars={5}
+            rating={rating}
+            fullStarColor={"#FEBF34"}
+            starSize={40}
+            starStyle={styles.rating}
+          />
+        </View>
+      </ScrollView>
+
+      <View style={styles.footer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.replace("Modifynote", {
+              note_id: note_id,
+              name: name,
+              origin: origin,
+              mall: mall,
+              price: price,
+              feature: feature,
+              rating: rating,
+              flavor: flavor,
+            })
+          }
+        >
+          <Text style={styles.bottonTextWhite}>수정하기</Text>
+        </TouchableOpacity>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            setModalVisible(true);
+          }}
+        >
+          <Text style={styles.bottonTextWhite}>삭제하기</Text>
+        </TouchableHighlight>
       </View>
 
-      <StarRating
-        disabled={true}
-        maxStars={5}
-        rating={rating}
-        fullStarColor={"#FEBF34"}
-      />
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() =>
-          navigation.replace("Modifynote", {
-            note_id: note_id,
-            name: name,
-            origin: origin,
-            mall: mall,
-            price: price,
-            feature: feature,
-            rating: rating,
-            flavor: flavor,
-          })
-        }
-      >
-        <Text>수정하기</Text>
-      </TouchableOpacity>
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
+          setModalVisible(false);
         }}
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text style={styles.modalText}>정말 삭제하시겠습니까?</Text>
-
+            <Text style={styles.modalWarningText}>
+              삭제시, 복구할 수 없습니다.
+            </Text>
             <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              style={styles.yesButton}
               onPress={() => {
                 deleteNoteInfo();
                 setModalVisible(!modalVisible);
               }}
             >
-              <Text>YES</Text>
+              <Text style={styles.bottonTextWhite}>YES</Text>
             </TouchableHighlight>
             <TouchableHighlight
-              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              style={styles.noButton}
               onPress={() => {
                 setModalVisible(!modalVisible);
               }}
             >
-              <Text>NO</Text>
+              <Text style={styles.modalNotext}>NO</Text>
             </TouchableHighlight>
           </View>
         </View>
       </Modal>
-
-      <TouchableHighlight
-        style={styles.button}
-        onPress={() => {
-          setModalVisible(true);
-        }}
-      >
-        <Text style={styles.textStyle}>삭제하기</Text>
-      </TouchableHighlight>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingLeft: 30,
+    // flexDirection: "row",
+    // flexWrap: "wrap",
+  },
+  container2: {
+    marginTop: 20,
+    marginBottom: 10,
+    width: 300,
+    borderBottomColor: "#000000",
+    borderBottomWidth: 5,
+  },
+  container3: {
+    marginTop: 12,
+    paddingBottom: 5,
+    width: 300,
+    borderBottomColor: "#E5E5E5",
+    borderBottomWidth: 1,
+  },
+  container4: {
+    marginTop: 5,
+    paddingBottom: 5,
+    width: 300,
     flexDirection: "row",
     flexWrap: "wrap",
   },
+  headtextstyle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    paddingBottom: 10,
+    color: "#692702",
+  },
+  topText: {
+    fontWeight: "800",
+    fontWeight: "bold",
+    fontSize: 15,
+    marginBottom: 10,
+    marginRight: 7,
+  },
+  bottomText: {
+    fontWeight: "400",
+    fontSize: 15,
+    width: 300,
+    marginRight: 20,
+  },
+  rating: { marginTop: 4 },
+  footer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    bottom: 0,
+    alignContent: "center",
+  },
+
   button: {
     alignItems: "center",
-    backgroundColor: "#DDDDDD",
+    backgroundColor: "#7B6D64",
+    borderRadius: 5,
     padding: 10,
-    marginBottom: 5,
+    width: 160,
+    height: 45,
+    margin: 5,
+  },
+  bottonTextWhite: {
+    fontWeight: "400",
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 16,
+  },
+  flavorbutton: {
+    alignItems: "center",
+    width: 70,
+    padding: 2,
+    margin: 2,
+    borderRadius: 5,
+    backgroundColor: "#E9E2E2",
   },
   centeredView: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     alignItems: "center",
     marginTop: 22,
   },
   modalView: {
     margin: 20,
     backgroundColor: "white",
-    borderRadius: 20,
-    padding: 60,
+    padding: 30,
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
@@ -244,16 +334,44 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+    width: 280,
+    height: 220,
   },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 15,
-    padding: 10,
-    elevation: 2,
+  yesButton: {
+    alignItems: "center",
+    borderRadius: 5,
+    backgroundColor: "#7B6D64",
+    padding: 8,
+    width: 200,
+    height: 40,
+    margin: 5,
+    marginTop: 10,
   },
   modalText: {
-    marginBottom: 15,
-    textAlign: "center",
+    fontWeight: "800",
+    fontSize: 15,
+    marginBottom: 10,
+    marginRight: 7,
+  },
+  noButton: {
+    alignItems: "center",
+    borderRadius: 5,
+    backgroundColor: "#D7D2CB",
+    padding: 8,
+    width: 200,
+    height: 40,
+    margin: 5,
+  },
+  modalWarningText: {
+    fontWeight: "300",
+    color: "red",
+    fontSize: 10,
+  },
+  modalNotext: {
+    fontWeight: "400",
+    fontWeight: "bold",
+    color: "#7F736B",
+    fontSize: 16,
   },
 });
 
